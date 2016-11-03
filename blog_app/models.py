132212570e039aa15abf2b1cb2d7339edda9004b
Kinga <kinga.kimbrel@gmail.com>
2016-11-02 21:29:07 -0500

    Initial commit

---
+++ blog_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Post(models.Model):
+    """A class representing a post."""
+    date_added = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.title + ": " + self.text[:50] + "..."
+
+
+class Comment(models.Model):
+    """Comments related to specific post."""
+    post = models.ForeignKey(Post)
+    date_added = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text[:50] + "..."
