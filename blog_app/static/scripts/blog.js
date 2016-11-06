711fc17744dcf2bdffa87b5bb686973ac5f2994e
Kinga <kinga.kimbrel@gmail.com>
2016-11-05 22:44:33 -0500

    First ajax call to get post by id

--- blog_app/static/scripts/blog.js
+++ blog_app/static/scripts/blog.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    $('#viewPost').on('click', function(){
+    var post_id = parseInt($(this).parent().find('#postID').val());
 
-    }
-);
+      $.ajax({ url: "/view_post/" + post_id + "/",
+                 type: 'GET',
+                 success: function(data) {
+                         $('#pageContent').html(data);
+                     },
+        });
+    });
+ });
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Post
 
 
@@ -11,6 +13,9 @@ def index(request):
 
 
 def view_post(request, post_id):
-    blog_post = Post.objects.get(id=post_id)
-    context = {'blog_post': blog_post}
-    return render(request, 'blog_app/view_post.html', context)
+    if request.is_ajax():
+        blog_post = Post.objects.get(id=post_id)
+        context = {'blog_post': blog_post}
+        return render(request, 'blog_app/view_post.html', context)
+
+    return HttpResponseRedirect(reverse('blog_app:index'))
